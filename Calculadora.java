e75aae605be6680a0e6384e8ccc48d8302269b54
elano-tavares <elano.tavares.ncs@gmail.com>
2025-09-15 19:24:55 -0300

    Implementação da Calculadora

--- Calculadora.java
+++ Calculadora.java
@@ -2,11 +2,33 @@ import java.util.Scanner;
 
 public class Calculadora {
 
-    /* área para a criação dos métodos*/
+    public int somar(int a, int b){
+        return a + b;
+    }
+
+    public int dividir(int numerador, int denominador){
+        return numerador / denominador;
+    }
     
     public static void main(String[] args) {
 
-      
+        Scanner ler = new Scanner(System.in);
+        
+        System.out.print("Primeiro valor: ");
+        int one = ler.nextInt();
+
+        System.out.print("Segundo valor: ");
+        int two = ler.nextInt();
+
+        Calculadora calc = new Calculadora();
+        int opSoma = calc.somar(one, two);
+        int opDividir = calc.dividir(one, two);
+
+        System.out.println("\n--- Resultados ---");
+        System.out.println("Soma: " + opSoma);
+        System.out.println("Divisão: " + opDividir);
+
+        ler.close();
 
     }
 }
